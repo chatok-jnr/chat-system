ec119c242f2565ad98ebf1c2755e4aa3cd79ca13
chatok-jnr <md.sakib.hos3n@gmail.com>
2026-09-14 17:15:25 +0600

    room concept

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
-import express from "express";
-import { createServer } from "node:http";
-import { fileURLToPath } from "node:url";
-import {dirname, join} from "node:path";
-import {Server} from "socket.io";
+import express from 'express';
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    connectionStateRecovery: {}
+});
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -15,13 +17,29 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
+
+    const { id:userId } = socket.handshake.headers;
+    console.log(`user-${userId} is connected`);
+
+    socket.join(`room-${userId}`);
+
     socket.on('chat message', (msg) => {
-        //io.emit('chat message', msg);
-        console.log(msg);
-        socket.broadcast.emit('chat message', msg);
+        console.log(`TargetUser: ${msg.toUserId}\nmessage: ${msg.text}`);
+
+        const payload = {};
+        payload.senderId = userId;
+        payload.message = msg.text;
+
+        socket.to(`room-${msg.toUserId}`).emit('chat message', payload);
+
     });
-})
+ 
+    socket.on('disconnect', (reason) => {
+        console.log(`user-${userId} is disconnected`);
+    });
+});
+
 
 server.listen(3000, () => {
-    console.log(`This server is running on  port 3000`);
+    console.log('server running at http://localhost:3000');
 });
